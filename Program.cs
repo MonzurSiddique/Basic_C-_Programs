dbfceeab1b4205cf7e97bcb788ca4a8921236b16
MonzurSiddique <mhsiddique@gmail.com>
2023-09-29 19:59:12 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace WelcomeMessage
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Account Systems");
+            string doubleQuotes = Console.ReadLine();
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
